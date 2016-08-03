0956f08790c0289b11cf78bce8e79b38c70158d8
krisrybak <kris.rybak@hotmail.co.uk>
2016-08-03 10:33:41 +0100

    Updates tests setup

---
+++ Tests/Fixtures/App/AppKernel.php
@@ -0,0 +1,40 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        return array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Symfony\Bundle\SecurityBundle\SecurityBundle(),
+            new Symfony\Bundle\TwigBundle\TwigBundle(),
+            new Symfony\Bundle\MonologBundle\MonologBundle(),
+            new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new RybakDigital\Api\FrameworkBundle\RybakDigitalApiFrameworkBundle(),
+        );
+    }
+
+    /** 
+     * @return string
+     */
+    public function getCacheDir()
+    {   
+        return sys_get_temp_dir().'/RybakDigitalApiFrameworkBundle/cache';
+    }   
+    /** 
+     * @return string
+     */
+    public function getLogDir()
+    {   
+        return sys_get_temp_dir().'/RybakDigitalApiFrameworkBundle/logs';
+    } 
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
